6c6088f0352cf886eca1ae89e19abdb3bf111877
Frits Boel <F.Boel@betabit.nl>
2023-10-29 11:01:19 +0100

    Added basic implementation for Maybe

--- src/iStruction.Monads/Maybe.cs
+++ src/iStruction.Monads/Maybe.cs
@@ -1,6 +1,34 @@
+using System;
+
+
 namespace iStruction.Monads
 {
-    public class Maybe
+    public static class Maybe
     {
+        public static Maybe<TItem> None<TItem>() => new Maybe<TItem>();
+
+        public static Maybe<TItem> Some<TItem>(this TItem item) => new Maybe<TItem>(item);
+    }
+
+
+    public readonly struct Maybe<TItem>
+    {
+        //private readonly bool _hasItem;
+        private readonly TItem _item;
+
+
+        public static Maybe<TItem> None() => new Maybe<TItem>();
+
+
+        internal Maybe(TItem item)
+        {
+            if (item == null)
+            {
+                throw new ArgumentNullException(nameof(item));
+            }
+
+            _item    = item;
+            //_hasItem = true;
+        }
     }
 }
--- test/iStruction.Monads.UnitTests/MaybeTest.cs
+++ test/iStruction.Monads.UnitTests/MaybeTest.cs
@@ -1,5 +1,7 @@
 using System.Diagnostics.CodeAnalysis;
 
+using iStruction.Monads.FluentAssertions;
+
 
 namespace iStruction.Monads.UnitTests;
 
@@ -7,7 +9,25 @@ namespace iStruction.Monads.UnitTests;
 public class MaybeTest
 {
     [Fact]
-    public void Test1()
+    public void Maybe_CanCreateNone()
     {
+        // Arrange
+        // Act
+        var result = Maybe.None<string>();
+
+        // Assert
+        result.Should().BeNone();
+    }
+
+
+    [Fact]
+    public void Maybe_CanCreateSome()
+    {
+        // Arrange
+        // Act
+        var result = "Somestring".Some();
+
+        // Assert
+        result.Should().BeSome();
     }
 }
